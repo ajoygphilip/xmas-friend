fdc1162909c2f60e92a267ffbb5599a2498496f7
ajoygp <41632101+ajoygp@users.noreply.github.com>
2023-12-10 16:19:14 +0530

    update view and serializers

--- example/serializers.py
+++ example/serializers.py
@@ -6,8 +6,7 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source="user.username", read_only=True)
-    friend = serializers.CharField(source="friend.username", read_only=True)
 
     class Meta:
         model = Profile
-        fields = ("id", "name", "friend", "has_selected", "has_been_selected")
+        fields = ("id", "name")
--- example/views.py
+++ example/views.py
@@ -36,5 +36,29 @@ def logout_view(request):
 
 
 class ProfileViewset(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Profile.objects.filter(friend=None).exclude(user=self.request.user)
+        else:
+            return Profile.objects.filter(friend=None)
+
+    @action(detail=True, methods=["post"])
+    def setFriend(self, request):
+        friend_id = request.data.get("friend_id")
+
+        if not friend_id:
+            return Response({"error": "friend_id is required"}, status=400)
+
+        try:
+            friend_profile = Profile.objects.get(user__id=friend_id)
+        except Profile.DoesNotExist:
+            return Response({"error": "Friend profile not found"}, status=404)
+
+        current_user_profile = self.get_queryset().get(user=request.user)
+
+        current_user_profile.friend = friend_profile
+        current_user_profile.save()
+
+        return Response({"success": "Friend set successfully"}, status=200)
