395cc6a851dd42a758a2fd808628b0cbbc327e1b
ajoy <ajoy.gp@gmail.com>
2023-12-13 10:06:37 +0530

    update logic

--- example/views.py
+++ example/views.py
@@ -5,6 +5,8 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -37,28 +39,58 @@ def logout_view(request):
 
 class ProfileViewset(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        print(self.request.user.profile.id)
+        print(self.request.user.profile.has_been_selected)
         if self.request.user.is_authenticated:
-            return Profile.objects.filter(friend=None).exclude(user=self.request.user)
+            return (
+                Profile.objects.filter(friend=None)
+                .exclude(user=self.request.user)
+                .filter(friend=None)
+            )
         else:
             return Profile.objects.filter(friend=None)
 
-    @action(detail=True, methods=["post"])
-    def setFriend(self, request):
+    def list(self, request, *args, **kwargs):
+        user = request.user
+        print(user.username)
+        if user.profile.has_selected:
+            return Response(
+                {
+                    "message": "you have already selected a friend",
+                    "friend": user.profile.friend.username,
+                }
+            )
+        else:
+            return super().list(request, *args, **kwargs)
+
+    @action(detail=False, methods=["post"])
+    def set_friend(self, request):
         friend_id = request.data.get("friend_id")
+
+        print("user =", request.user.id)
+        print("friend =", friend_id)
 
         if not friend_id:
             return Response({"error": "friend_id is required"}, status=400)
 
+        if friend_id == request.user.id:
+            return Response({"error": "you cannot be your own friend"}, status=400)
+
         try:
-            friend_profile = Profile.objects.get(user__id=friend_id)
+            friend_id = int(friend_id)
+            friend_profile = Profile.objects.get(id=friend_id)
         except Profile.DoesNotExist:
             return Response({"error": "Friend profile not found"}, status=404)
 
-        current_user_profile = self.get_queryset().get(user=request.user)
-
-        current_user_profile.friend = friend_profile
+        current_user_profile = request.user.profile
+        current_user_profile.friend = friend_profile.user
+        current_user_profile.has_selected = True
         current_user_profile.save()
+
+        friend_profile.has_been_selected = True
+        friend_profile.save()
 
         return Response({"success": "Friend set successfully"}, status=200)
